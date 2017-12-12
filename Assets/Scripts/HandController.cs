5a1e378a190c7b37729020dda276680c1d370179
hang1038 <31604301+hang1038@users.noreply.github.com>
2017-12-12 20:46:28 +0800

    ~

---
+++ Assets/Scripts/HandController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandController : MonoBehaviour {
+    private Valve.VR.EVRButtonId grip = Valve.VR.EVRButtonId.k_EButton_Grip;
+    private Valve.VR.EVRButtonId trigger = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+
+    private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObject.index); } }
+    private SteamVR_TrackedObject trackedObject;
+
+    HashSet<Interactable> interactable = new HashSet<Interactable>();
+
+    private Interactable interactingItem;
+
+	// Use this for initialization
+	void Start () {
+        trackedObject = GetComponent<SteamVR_TrackedObject>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (controller.GetPressDown(grip))
+        {
+            float minDistance = float.MaxValue;
+            float distance;
+
+            foreach (Interactable item in interactable)
+            {
+                distance = (item.transform.position - transform.position).sqrMagnitude;
+
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                    interactingItem = item;
+                }
+            }
+
+            if (interactingItem)
+            {
+                if (interactingItem.IsInteracting())
+                {
+                    interactingItem.EndInteraction(this);
+                }
+
+                interactingItem.StartInteraction(this);
+            }
+        }
+
+        if (controller.GetPressUp(grip) && interactingItem != null)
+        {
+            interactingItem.EndInteraction(this);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Interactable collider = other.GetComponent<Interactable>();
+        if (collider)
+        {
+            interactable.Add(collider);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Interactable collider = other.GetComponent<Interactable>();
+        if (collider)
+        {
+            interactable.Remove(collider);
+        }
+    }
+}
---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour {
+    public Rigidbody rigidbody;
+
+    private bool interacting;
+
+    private float speed = 20000f;
+    private Vector3 positionDelta;
+
+    private float angularSpeed = 400f;
+    private Quaternion rotationDelta;
+    private float angle;
+    private Vector3 axis;
+
+    private HandController hand;
+
+    private Transform interactionPoint;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+        interactionPoint = new GameObject().transform;
+        speed /= rigidbody.mass;
+        angularSpeed /= rigidbody.mass;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (hand && interacting)
+        {
+            positionDelta = hand.transform.position - interactionPoint.position;
+            this.rigidbody.velocity = positionDelta * speed * Time.fixedDeltaTime;
+
+            rotationDelta = hand.transform.rotation * Quaternion.Inverse(interactionPoint.rotation);
+            rotationDelta.ToAngleAxis(out angle, out axis);
+
+            if (angle > 180)
+            {
+                angle -= 360;
+            }
+
+            this.rigidbody.angularVelocity = (Time.fixedDeltaTime * angle * axis) * angularSpeed;
+        }
+	}
+
+    public void StartInteraction(HandController hand)
+    {
+        this.hand = hand;
+        interactionPoint.position = hand.transform.position;
+        interactionPoint.rotation = hand.transform.rotation;
+        interactionPoint.SetParent(transform, true);
+        interacting = true;
+    }
+
+    public void EndInteraction(HandController hand)
+    {
+        if (this.hand == hand)
+        {
+            this.hand = null;
+            interacting = false;
+        }
+    }
+
+    public bool IsInteracting()
+    {
+        return interacting;
+    }
+}
